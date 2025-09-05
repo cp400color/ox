7a0fac79ff7cd7e71d33ea75d2809e901e98d817
cp400color <felipe-sdaudt@estudante.rs.gov.br>
2025-09-05 14:03:46 +0000

    Make run run the script

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -1,3 +1,4 @@
+use std::process::Command;
 use anstyle::Style;
 
 fn get_script_path(script: Option<&String>, subscript: Option<&String>) -> String {
@@ -11,6 +12,13 @@ fn get_script_path(script: Option<&String>, subscript: Option<&String>) -> Strin
     return format!("{}{}{}", root, folder, file)
 }
 
+pub fn run(script: Option<&String>, subscript: Option<&String>) -> () {
     const BOLD_STYLE: Style = Style::new().bold();
 
+    let path: String = get_script_path(script, subscript);
+    println!("Running {BOLD_STYLE}{path}{BOLD_STYLE:#}...");
+
+    Command::new(path)
+        .output()
+        .expect("failed to execute process");
 }
