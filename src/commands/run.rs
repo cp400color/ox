06ddbbed8ad6217d6cdc5f552f04e4a1bf862de2
fgclue <lost@biitle.nl>
2025-09-05 14:45:12 -0300

    Make run use args

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use clap::parser::ValuesRef;
 use anstyle::Style;
 
 fn get_script_path(script: Option<&String>, subscript: Option<&String>) -> String {
@@ -12,13 +13,19 @@ fn get_script_path(script: Option<&String>, subscript: Option<&String>) -> Strin
     return format!("{}{}{}", root, folder, file)
 }
 
-pub fn run(script: Option<&String>, subscript: Option<&String>) -> () {
+pub fn run(script: Option<&String>, subscript: Option<&String>, extras: Option<ValuesRef<'_, std::string::String>>) -> () {
     const BOLD_STYLE: Style = Style::new().bold();
 
     let path: String = get_script_path(script, subscript);
     println!("Running {BOLD_STYLE}{path}{BOLD_STYLE:#}...");
 
+    let args = match extras {
+        Some(_) => extras.unwrap().collect(),
+        None => vec!()
+    };
+
     Command::new(path)
+        .args(args)
         .output()
         .expect("failed to execute process");
 }
