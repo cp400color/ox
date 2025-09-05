e41bc91f042abad44e5c3fd7ec86bac855328d33
cp400color <felipe-sdaudt@estudante.rs.gov.br>
2025-09-05 11:17:51 +0000

    Add function to get path of script

--- src/commands/run.rs
+++ src/commands/run.rs
@@ -1,7 +1,14 @@
-fn get_script_path() {
-    
+fn get_script_path(script: Option<&String>, subscript: Option<&String>) -> String {
+    let root = ".ox/";
+    let folder = script.unwrap();
+    let file = match subscript {
+        Some(script) => format!("/{}", script),
+        None => "".to_string()
+    };
+
+    return format!("{}{}{}", root, folder, file)
 }
 
 pub fn run(script: Option<&String>, subscript: Option<&String>) {
-
+    let path = get_script_path(script, subscript);
 }
