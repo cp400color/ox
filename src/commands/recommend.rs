10f850c90e531459e6febacec036834d3fa69821
cp400color <229968751+cp400color@users.noreply.github.com>
2025-09-15 14:44:45 +0000

    Read oxfile in recommend.rs

--- src/commands/recommend.rs
+++ src/commands/recommend.rs
@@ -1,3 +1,20 @@
-pub fn recommend() {
+use std::io::Read;
+use std::path::Path;
+use std::fs::File;
 
+pub fn recommend() {
+    let path = Path::new(".ox/oxfile");
+    
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't recommend anything {}", why),
+        Ok(file) => file,
+    };
+
+    let mut oxfile = String::new();
+    match file.read_to_string(&mut oxfile) {
+        Err(why) => panic!("couldn't recommend anything {}", why),
+        Ok(_) => (),
+    };
+
+    println!("{:?}", oxfile) // do something here
 }
